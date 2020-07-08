d1dca1c34f9e30c5482ecd33d5a72e74dd1bfd84
Gabriela johnson <johnsongabriela12@gmail.com>
2020-07-08 21:40:26 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,17 @@
+
+
+#def square_array(numbers)
+  #new_array[]
+  #numbers.each do |number|
+   # new_array << number ** 2
+  #end
+ # new_array
+#end 
 def square_array(array)
-  # your code here
+  squared_array = []
+  array.each do |num|
+    square = num ** 2
+    squared_array << square
+  end
+  squared_array
 end
