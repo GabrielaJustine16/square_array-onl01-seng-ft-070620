48b8b8191e752a2f9918c7aa08657c120fdd89d1
Gabriela johnson <johnsongabriela12@gmail.com>
2020-07-08 21:39:30 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,17 @@
 
 
-def square_array(numbers)
-  new_array[]
-  numbers.each do |number|
-    new_array << number ** 2
-  end
-  new_array
+#def square_array(numbers)
+  #new_array[]
+  #numbers.each do |number|
+   # new_array << number ** 2
+  #end
+ # new_array
 end 
+def square_array(array)
+  squared_array = []
+  array.each do |num|
+    square = num ** 2
+    squared_array << square
+  end
+  squared_array
+end
